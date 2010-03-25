c491be4d7ef60bfd3af7b34e69a70ff967a22427
Marcel Grünauer <marcel@cpan.org>
2010-03-25 21:26:34 +0000

    Import of MARCEL/Data-Domain-Net-1.100840 from CPAN.
    gitpan-cpan-distribution: Data-Domain-Net
    gitpan-cpan-version:      1.100840
    gitpan-cpan-path:         MARCEL/Data-Domain-Net-1.100840.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Data/Domain/Net.pm
+++ lib/Data/Domain/Net.pm
@@ -1,47 +1,32 @@
-package Data::Domain::Net;
-
+use 5.008;
 use strict;
 use warnings;
+
+package Data::Domain::Net;
+our $VERSION = '1.100840';
+# ABSTRACT: Data domain classes for IP addresses
 use Data::Domain::SemanticAdapter;
-
-
-our $VERSION = '0.02';
-
-
-use base 'Exporter';
-
-
+use Exporter qw(import);
 our %map = (
     IPv4 => 'Net::IPAddress::IPv4',
     IPv6 => 'Net::IPAddress::IPv6',
 );
-
-
-our %EXPORT_TAGS = (
-    util => [ keys %map ],
-);
-
-
+our %EXPORT_TAGS = (util => [ keys %map ],);
 our @EXPORT_OK = @{ $EXPORT_TAGS{all} = [ map { @$_ } values %EXPORT_TAGS ] };
-
-
 Data::Domain::SemanticAdapter::install_shortcuts(%map);
-
-
 1;
 
 
 __END__
-
-
+=pod
 
 =head1 NAME
 
 Data::Domain::Net - Data domain classes for IP addresses
 
-=head1 SYNOPSIS
+=head1 VERSION
 
-    Data::Domain::Net->new;
+version 1.100840
 
 =head1 DESCRIPTION
 
@@ -79,59 +64,39 @@ Arguments are passed on to the object's constructor.
 
 By using the C<:all> tag, you can import all of them.
 
-Data::Domain::Net inherits from L<Exporter>.
+=head1 INSTALLATION
 
-The superclass L<Exporter> defines these methods and functions:
-
-    as_heavy(), export(), export_fail(), export_ok_tags(), export_tags(),
-    export_to_level(), import(), require_version()
-
-=head1 METHODS
-
-=over 4
-
-
-
-=back
-
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<datadomainnet> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.02 of L<Data::Domain::Net>.
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<<bug-data-domain-net@rt.cpan.org>>, or through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Data-Domain-Net>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Data-Domain-Net/>.
+
+The development version lives at
+L<http://github.com/hanekomu/Data-Domain-Net/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
--- lib/Data/Domain/Net/IPAddress/IPv4_TEST.pm
+++ lib/Data/Domain/Net/IPAddress/IPv4_TEST.pm
@@ -1,153 +1,81 @@
-package Data::Domain::Net::IPAddress::IPv4_TEST;
-
+use 5.008;
 use strict;
 use warnings;
+
+package Data::Domain::Net::IPAddress::IPv4_TEST;
+our $VERSION = '1.100840';
+# ABSTRACT: Test companion class for the IPv4 address data domain class
 use Data::Domain::Net ':all';
 use Test::More;
-
-
-our $VERSION = '0.02';
-
-
-use base qw(
-    Data::Domain::SemanticAdapter::Test
-    Data::Semantic::Net::IPAddress::TestData::IPv4
+use parent qw(
+  Data::Domain::SemanticAdapter::Test
+  Data::Semantic::Net::IPAddress::TestData::IPv4
 );
-
-
 use constant PLAN => 5;
 
 sub run {
     my $self = shift;
     $self->SUPER::run(@_);
-
     my $domain = IPv4();
-
-    isa_ok($domain, 'Data::Domain::Net::IPAddress::IPv4');
-    isa_ok($domain, 'Data::Domain::SemanticAdapter');
+    isa_ok($domain,          'Data::Domain::Net::IPAddress::IPv4');
+    isa_ok($domain,          'Data::Domain::SemanticAdapter');
     isa_ok($domain->adaptee, 'Data::Semantic::Net::IPAddress::IPv4');
-
-    my $domain2 = IPv4(-not_in => [ qw(212.186.17.4 212.186.17.5) ]);
+    my $domain2 = IPv4(-not_in => [qw(212.186.17.4 212.186.17.5)]);
     $self->is_excluded($domain2, '212.186.17.4');
     $self->is_valid($domain2, '212.186.17.6');
 }
-
-
 1;
 
 
 __END__
-
+=pod
 
 =head1 NAME
 
-Data::Domain::Net::IPAddress::IPv4_TEST - Test companion class
+Data::Domain::Net::IPAddress::IPv4_TEST - Test companion class for the IPv4 address data domain class
 
-=head1 SYNOPSIS
+=head1 VERSION
 
-    perl t/01_companion_classes.t
+version 1.100840
 
 =head1 DESCRIPTION
 
 Test companion class for L<Data::Domain::Net::IPAddress::IPv4>. Gets its
 test data from L<Data::Semantic::Net::IPAddress::TestData::IPv4>.
 
-Data::Domain::Net::IPAddress::IPv4_TEST inherits from
-L<Data::Domain::SemanticAdapter::Test> and
-L<Data::Semantic::Net::IPAddress::TestData::IPv4>.
+=head1 INSTALLATION
 
-The superclass L<Data::Domain::SemanticAdapter::Test> defines these methods
-and functions:
-
-    hash_rename(), is_excluded(), is_invalid(), is_valid(), munge_args(),
-    test_is_invalid(), test_is_valid()
-
-The superclass L<Test::CompanionClasses::Base> defines these methods and
-functions:
-
-    new(), clear_package(), make_real_object(), package(), package_clear(),
-    planned_test_count()
-
-The superclass L<Class::Accessor::Complex> defines these methods and
-functions:
-
-    carp(), cluck(), croak(), flatten(), mk_abstract_accessors(),
-    mk_array_accessors(), mk_boolean_accessors(),
-    mk_class_array_accessors(), mk_class_hash_accessors(),
-    mk_class_scalar_accessors(), mk_concat_accessors(),
-    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
-    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
-    mk_set_accessors(), mk_singleton()
-
-The superclass L<Class::Accessor> defines these methods and functions:
-
-    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
-    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
-    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
-    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
-    mk_wo_accessors(), mutator_name_for(), set()
-
-The superclass L<Class::Accessor::Installer> defines these methods and
-functions:
-
-    install_accessor(), subname()
-
-The superclass L<Data::Inherited> defines these methods and functions:
-
-    every_hash(), every_list(), flush_every_cache_by_key()
-
-The superclass L<Data::Semantic::Net::IPAddress::TestData::IPv4> defines
-these methods and functions:
-
-    TESTDATA()
-
-=head1 METHODS
-
-=over 4
-
-
-
-=back
-
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<datadomainnet> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.02 of L<Data::Domain::Net::IPAddress::IPv4_TEST>.
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<<bug-data-domain-net@rt.cpan.org>>, or through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Data-Domain-Net>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Data-Domain-Net/>.
+
+The development version lives at
+L<http://github.com/hanekomu/Data-Domain-Net/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
-
 
